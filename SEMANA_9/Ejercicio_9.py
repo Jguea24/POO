19fc51b5316d33973c9d61db91d4444cd99c2f06
Johnny <jg.grefac@uea.edu.ec>
2024-08-23 13:51:49 -0500

    Actualizado

--- SEMANA_9/Ejercicio_9.py
+++ SEMANA_9/Ejercicio_9.py
@@ -1,3 +1,4 @@
+
 """  Desarrollar un sistema de gestión de inventarios simple para una tienda, que permita añadir, actualizar,
 eliminar y buscar productos utilizando una estructura de datos personalizada."""
 
